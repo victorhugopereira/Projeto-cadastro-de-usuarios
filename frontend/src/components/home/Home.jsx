e7b0921633abe446122e8fa9c48a2a66a21b4be0
Victor Hugo <darke.sos.1999@gmail.com>
2021-11-24 14:30:25 -0300

    primeiro envio

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início"
+        subtitle="Segundo projeto do capitulo react">
+        <div className='display-4'>Bem vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema para exemplificar a construção de um
+        cadastro desenvolvido em React!</p>
+        <h1></h1>
+    </Main>
---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            developed with<i className="fa fa-heart text-danger"></i> per <strong>darke help</strong>
+        </span>
+    </footer>
