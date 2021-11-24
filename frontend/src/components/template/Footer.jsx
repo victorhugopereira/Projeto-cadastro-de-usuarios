8556eb826fa55668e725a32dcf0980e72bbc2694
Victor hugo <darke.sos.1999@gmail.com>
2021-11-24 14:50:15 -0300

    Update Footer.jsx

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -4,6 +4,6 @@ import React from 'react'
 export default props =>
     <footer className="footer">
         <span>
-            developed with<i className="fa fa-heart text-danger"></i> per <strong>darke help</strong>
+            developed with<i className="fa fa-heart text-danger"></i> per <strong>Victor hugo</strong>
         </span>
     </footer>
